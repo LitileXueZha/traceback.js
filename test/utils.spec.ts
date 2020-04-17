253565704841243ca43560fe7adbddf629899d05
诸葛林 <1941639715@qq.com>
2020-04-17 18:18:41 +0800

    提交

---
+++ test/utils.spec.ts
@@ -0,0 +1,9 @@
+import { should } from 'chai';
+
+import { parseRows } from '../src/utils';
+
+describe('通用函数', () => {
+    it('parseRows', () => {
+        console.log(parseRows(-1))
+    });
+});
